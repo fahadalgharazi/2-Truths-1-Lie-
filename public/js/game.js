40ddeb5ea5da266b3b418c8e701c8ae7a6efb040
Eric Schmidt <em.schmidt78@gmail.com>
2021-08-04 21:28:34 +0000

    fix: removes duplication

--- public/js/game.js
+++ public/js/game.js
@@ -4,99 +4,81 @@
             //display user 2 truths and a lie
                 //randomize location of the two truths and lie
 
+let googleUser;
+
 window.onload = (event) => {
   // Use this to retain user state between html pages.
-  firebase.auth().onAuthStateChanged(function(user) {
+  firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-      console.log('Logged in as: ' + user.displayName);
+      console.log("Logged in as: " + user.displayName);
       googleUser = user.uid;
-      console.log(googleUser)
+      console.log(googleUser);
       getSubmission(googleUser);
-
     } else {
-    // If not logged in, navigate back to login page.
-      window.location = 'index.html'; 
-    // console.log("not signed in");
-
-    };
+      // If not logged in, navigate back to login page.
+      window.location = "index.html";
+      // console.log("not signed in");
+    }
   });
 };
 
 const getSubmission = () => {
-    console.log("getting submissions")
+  document.querySelector("#gameSection").innerHTML = "";
+
+  console.log("getting submissions");
   const userRef = firebase.database().ref(`users`);
-  userRef.on('value', (data) => {
+  userRef.on("value", (data) => {
     const userData = data.val();
     // renderDataAsHtml(data);
-    var userKeys = Object.keys(userData)
+    var userKeys = Object.keys(userData);
     console.log(userKeys);
-    
-    
-        
-
-    userKeys.forEach((userKey)=>{
-        if(userKey != googleUser){
-/*            
-        // const userInfo = firebase.database().ref(`users/${userKey}`);
-        // userInfo.on('value',snapshot =>{
-        const userInfo = snapshot.val();
-            console.log("1")
-            renderDataAsHtml(userInfo);
-            console.log(userInfo);
-        // })
-        */
-
+    userKeys.forEach((userKey) => {
+      if (userKey != googleUser) {
         const userInfo = userData[userKey];
         console.log(userInfo);
         renderDataAsHtml(userInfo);
       }
     });
-
   });
 };
 
-
-
 const renderDataAsHtml = (data) => {
   let cards = ``;
-  for(const submissionKey in data) {
-      if(submissionKey != googleUser){
-      console.log(submissionKey)
+  for (const submissionKey in data) {
+    console.log(submissionKey);
     const submissionText = data[submissionKey];
-    cards += createCard(submissionText, submissionKey)
-  };
+    cards += createCard(submissionText, submissionKey);
   }
-  document.querySelector('#gameSection').innerHTML = cards;
+  document.querySelector("#gameSection").innerHTML += cards;
 };
 
-  let innerHTML = "";
-
 const createCard = (submissionText, submissionKey) => {
-  innerHTML += `<div class="column is-one-quarter">`
-  innerHTML += `<div class="card">`
-  innerHTML += `<header class="card-header">`
-  innerHTML += `<p class="card-header-title">`
-  innerHTML += `2 truths and a lie`
-  innerHTML += `</p>`
-  innerHTML += `</header>`
-  innerHTML += `<div class="card-content">`
-  innerHTML += `<div class="content">`
-  innerHTML += `${submissionText.truth1}`
-  innerHTML += `</div>`
-  innerHTML += `<div class="content">`
-  innerHTML += `${submissionText.truth2}`
-  innerHTML += `</div>`
-  innerHTML += `<div class="content">`
-  innerHTML += `${submissionText.lie}`
-  innerHTML += `</div>`
-  innerHTML += `</div>`
-  innerHTML +=  `<footer class="card-footer">`
-//   innerHTML +=  `<a id="${submissionKey}" href="#" class="card-footer-item" onclick="editNote(this.id)">Truth</a>`
-  innerHTML += `<br>`
-//   innerHTML +=  `<a id="${submissionKey}" href="#" class="card-footer-item" onclick="deleteNote(this.id)">Lie</a>`
-  innerHTML +=  `</footer>`
-  innerHTML += `</div>`
-  innerHTML += `</div>`
+  let innerHTML = "";
+  innerHTML += `<div class="column is-one-quarter">`;
+  innerHTML += `<div class="card">`;
+  innerHTML += `<header class="card-header">`;
+  innerHTML += `<p class="card-header-title">`;
+  innerHTML += `2 truths and a lie`;
+  innerHTML += `</p>`;
+  innerHTML += `</header>`;
+  innerHTML += `<div class="card-content">`;
+  innerHTML += `<div class="content">`;
+  innerHTML += `${submissionText.truth1}`;
+  innerHTML += `</div>`;
+  innerHTML += `<div class="content">`;
+  innerHTML += `${submissionText.truth2}`;
+  innerHTML += `</div>`;
+  innerHTML += `<div class="content">`;
+  innerHTML += `${submissionText.lie}`;
+  innerHTML += `</div>`;
+  innerHTML += `</div>`;
+  innerHTML += `<footer class="card-footer">`;
+  //   innerHTML +=  `<a id="${submissionKey}" href="#" class="card-footer-item" onclick="editNote(this.id)">Truth</a>`
+  innerHTML += `<br>`;
+  //   innerHTML +=  `<a id="${submissionKey}" href="#" class="card-footer-item" onclick="deleteNote(this.id)">Lie</a>`
+  innerHTML += `</footer>`;
+  innerHTML += `</div>`;
+  innerHTML += `</div>`;
 
   return innerHTML;
 };
